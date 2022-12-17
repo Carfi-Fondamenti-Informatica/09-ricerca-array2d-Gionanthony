daa81cec04c2b84443479ff2f40c769462f58c9f
Gionanthony <117936434+Gionanthony@users.noreply.github.com>
2022-12-17 09:32:06 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,5 +1,30 @@
 #include <iostream>
+#include "lib.h"
+using namespace std;
 
-int main(){
-  return 0;
+int main() {
+    int c=0;
+    char a[10][20], b[20];
+
+    for (int x=0; x<10; x++) {
+        for (int y=0; y<20; y++) {
+            b[y] = ' ';
+            a[x][y] = ' ';
+        }
+    }
+
+
+    for (int i=0; i<10; i++) {
+        cin >> a[i];
+    }
+
+    cin >> b;
+
+    if(funzione(a,b,c)) {
+        cout << c;
+    } else {
+        cout << "non presente";
+    }
+
+    return 0;
 }
