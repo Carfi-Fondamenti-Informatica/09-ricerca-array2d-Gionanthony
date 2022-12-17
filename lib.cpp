48faef476c7960dc78e5764952ae72f5651535be
Gionanthony <117936434+Gionanthony@users.noreply.github.com>
2022-12-17 09:31:33 +0100

    Update lib.cpp

--- lib.cpp
+++ lib.cpp
@@ -1,1 +1,17 @@
+#include "lib.h"
 
+bool funzione (char a[10][20], char b[], int &c) {
+
+    for (int x=0; x<10; x++) {
+        for (int y=0; y<20; y++) {
+            if (a[x][y] != b[y]) {
+                y = 20;
+            } else if (y==19) {
+                c = x;
+                return true;
+            }
+        }
+    }
+
+    return false;
+}
